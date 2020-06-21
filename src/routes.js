f4c9d8575b9d62a5969880a25d191a7dc333a040
brenu <bvsousa64@gmail.com>
2020-06-21 01:37:02 -0300

    :rocket: Stablish connection with the database and add 'create fake user' route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-
-import Database from './config/database';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'brenu',
+    email: 'brenu@brenu.com',
+    password_hash: 'itsnotahash',
+  });
+
+  res.json(user);
 });
 
 export default routes;
