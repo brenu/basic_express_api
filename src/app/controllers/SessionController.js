40cbf018b85347f1426bab188f38d91a8428815a
brenu <bvsousa64@gmail.com>
2020-06-22 00:50:55 -0300

    :art: Create separate file for authentication config

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,9 @@ import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
 
+import authConfig from '../../config/auth';
+import auth from '../../config/auth';
+
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
@@ -24,8 +27,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, '87ae25c8b87c3d956cc7993d90f00c67', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '87ae25c8b87c3d956cc7993d90f00c67',
+  expiresIn: '7d',
+};
