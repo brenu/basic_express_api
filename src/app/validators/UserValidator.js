a2bb515fed380aefc0bc8fcbdd8b96c7fe883e9e
brenu <bvsousa64@gmail.com>
2020-06-24 02:39:06 -0300

    :art: Create isolated user validators file

---
+++ src/app/validators/UserValidator.js
@@ -0,0 +1,41 @@
+import * as Yup from 'yup';
+
+class UserValidator {
+  async store(req, res, next) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    return next();
+  }
+
+  async update(req, res, next) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    return next();
+  }
+}
+
+export default new UserValidator();
--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,18 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import UserValidator from './app/validators/UserValidator';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
+routes.post('/users', UserValidator.store, UserController.store);
 
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update);
+routes.put('/users', UserValidator.update, UserController.update);
 
 export default routes;
